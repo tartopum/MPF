b5bf7563330b8e999ed520d82546a07c1ede665f
vincent <vincent.lefoulon@free.fr>
2015-01-25 13:48:58 +0100

    Use absolute imports

--- mpf/processors/__init__.py
+++ mpf/processors/__init__.py
@@ -1,6 +1,6 @@
-from .alea_values import AleaValues
-from .difference import Difference
-from .fourier import ImFourierTransform, ReFourierTransform
-from .linear_regression import LinearRegression
-from .moving_averaging import MovingAveraging
-from .statistics import Statistics
+from mpf.processors.alea_values import AleaValues
+from mpf.processors.difference import Difference
+from mpf.processors.fourier import ImFourierTransform, ReFourierTransform
+from mpf.processors.linear_regression import LinearRegression
+from mpf.processors.moving_averaging import MovingAveraging
+from mpf.processors.statistics import Statistics
