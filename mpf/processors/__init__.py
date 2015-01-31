5b8e0ef83bbebc8cb492b0b411939a8a2cac8811
vincent <vincent.lefoulon@free.fr>
2015-01-31 16:11:04 +0100

    Fix name error

--- mpf/processors/__init__.py
+++ mpf/processors/__init__.py
@@ -1,6 +1,6 @@
-from mpf.processors.alea_values import AleaValues
+from mpf.processors.aleavalues import AleaValues
 from mpf.processors.difference import Difference
 from mpf.processors.fourier import ImFourierTransform, ReFourierTransform
-from mpf.processors.linear_regression import LinearRegression
-from mpf.processors.moving_averaging import MovingAveraging
+from mpf.processors.linearregression import LinearRegression
+from mpf.processors.movingaveraging import MovingAveraging
 from mpf.processors.statistics import Statistics
