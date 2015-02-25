d502185b4468fd16d81f24e3a0e6c3e58bd84f04
vincent.lefoulon <vincent.lefoulon@b313-p08>
2015-02-25 15:14:21 +0100

    Clean data

--- data/database/sql/tables.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS CrudeData(
-    cow INTEGER NOT NULL,
-    date DATE NOT NULL,
-    prod REAL NOT NULL, -- L
-    cons REAL NOT NULL, -- kg
-    lact INTEGER NOT NULL,
-    day INTEGER NOT NULL,
-    PRIMARY KEY (cow, date)
-);
