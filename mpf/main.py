6a3b430cf20a45792947e26732e7d1c3a64cc8dd
Vayel <vincent.lefoulon@free.fr>
2015-05-23 18:58:31 +0200

    Generate smoothed data

--- mpf/main.py
+++ mpf/main.py
@@ -1,12 +1,15 @@
+"""The starting point of the code. Run it to make the analysis work."""
+
 from mpf import analysis
 # from mpf import views
 from mpf import tools
 
 
 def main():
+    """Launch the analysis."""
+
     for cow in tools.db.cows():
         analysis.MovingAveraging(step=2).work(cow)
-        break
 
 
 if __name__ == '__main__':
