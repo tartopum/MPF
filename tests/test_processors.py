97b8933ffb6eca9b29f1a33d28dcb165d51f9bde
Vayel <vincent.lefoulon@free.fr>
2015-05-24 14:28:25 +0200

    Clean tests

--- tests/test_processors.py
+++ tests/test_processors.py
@@ -1,21 +1,21 @@
-from mpf.processors import alea, diff, linreg, ma, stats 
+"""Test the processors."""
+
+from mpf.processors import diff, ma
 
 
 # Difference
-def test_difference():
+def test_diff_difference():
     data = [1, 2, 3, 4, 5]
-    assert diff.process(data=data) == [2-1, 3-2, 4-3, 5-4]
-    
 
-# Alea values
-def test_alea_values():
-    data = [[1, 2, 3], [4, 5, 6]]
+    assert diff.difference(data=data) == [2-1, 3-2, 4-3, 5-4]
     
-    alea.process(data=data, percentage=80)
-    
-    
+def test_diff_truncate():
+    data = [1, 2, 3, 4, 5]
+
+    assert diff.truncate(data=data) == [2, 3, 4, 5]
+
 # MovingAveraging
-def test_smooth():
+def test_ma_smooth():
     step = 1
     d = 2*step + 1
     data = [10, 15, 8, 3, 14, 7]
@@ -23,38 +23,8 @@ def test_smooth():
     assert ma.smooth(data=data, step=step) == [(10+15+8)/d, (15+8+3)/d, 
                                                (8+3+14)/d, (3+14+7)/d]
                                                
-def test_truncate():
+def test_ma_truncate():
     step = 2
     data = [1, 2, 3, 4, 5, 6, 7]
     
     assert ma.truncate(data=data, step=step) == [3, 4, 5]
-                                               
-                                               
-# LinearRegression
-def test_linreg():
-    A = [
-        [1, 9, 9**2, 4, 4**2],
-        [1, 8, 8**2, 6, 6**2],
-        [1, 9, 9**2, 4, 4**2],
-        [1, 3, 3**2, 7, 7**2],
-        [1, 6, 6**2, 8, 8**2],
-        [1, 4, 4**2, 5, 5**2]
-    ]
-    
-    B = [9, 10, 2, 4, 2, 10]
-    
-    X = linreg.process(A=A, B=B)
-    error = linreg.error(X=X, A=A, B=B)
-    
-    
-# Statistics
-def test_stats():
-    data = [1, 2, 3, 4, 4]
-    
-    assert stats.process(data=data) == {
-                                        "mean": 2.8,
-                                        "median": 3,
-                                        "mode": 4,
-                                        "variance": 1.7
-                                    }
-    
